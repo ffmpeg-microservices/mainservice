477b1bffa5e108169a2eae9135e064acee310392
Raghav Sharma <tilakddsharma@gmail.com>
2026-02-24 12:54:31 +0530

    Added gif in media type

--- src/main/java/com/mediaalterations/mainservice/dto/MediaType.java
+++ src/main/java/com/mediaalterations/mainservice/dto/MediaType.java
@@ -2,5 +2,6 @@ package com.mediaalterations.mainservice.dto;
 
 public enum MediaType {
     mp3, aac, wav, flac, ogg, m4a,
-    mp4, avi, mkv, mov, wmv, flv, webm, mpeg, mpg, m4v // 3gp
+    mp4, avi, mkv, mov, wmv, flv, webm, mpeg, mpg, m4v, // 3gp
+    gif
 }
